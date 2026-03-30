3ffa9570d443e75afa7be94f2229615156743359
teobotindariudesa <bteo@udesa.edu.ar>
2026-03-30 11:01:36 -0300

    Funcionamiento del switch, creacion de screens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Footer from "./components/Footer/Footer.js";
 import Header from "./components/Header/Header.js";
 import {Switch, Route} from "react-router-dom"
+import Home from "./screen/Home/Home.js"
 
 
 function App() {
   return (
     <div>
+      <div className="container">
       <Header/>
+      <Switch>
+        <Route path="/" exact={true} component={Home}/>
+      </Switch>
+      </div>
       <Footer/> 
     </div>
   );
--- src/screen/Home/Home.js
+++ src/screen/Home/Home.js
@@ -1,1 +1,9 @@
-    
+import React from "react";
+
+function Home() {
+    return(
+    <h2>ACA VA EL HOME</h2>
+    )
+}
+
+export default Home
