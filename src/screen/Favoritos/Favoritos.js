15f5e0e22d7719d385064da0233b2bc068b60999
tpaladino-a11y <tpaladino@udesa.edu.ar>
2026-04-18 16:09:22 -0300

    Cambio en logica del favorito

--- src/screen/Favoritos/Favoritos.js
+++ src/screen/Favoritos/Favoritos.js
@@ -10,83 +10,73 @@ class Favoritos extends Component {
     };
   }
 
-  //La idea es agarrar del localstorage todos los favoritos y meterlos en el state de este compoennte para ahi trabajarlos y renderizarlos
   componentDidMount() {
-    // Traigo lo que ya hay o nada de favoritos en el storage. 
     const storage = localStorage.getItem("favoritos");
 
     if (storage !== null) {
-
-      // Si si hay algo, lo parseo
       const favoritosGuardados = JSON.parse(storage);
 
-      // Por cada favorito hago fetch a la API para tener la data completa
-      Promise.all(
-        favoritosGuardados.map(fav =>
-          fetch(`https://api.themoviedb.org/3/${fav.tipo}/${fav.id}?api_key=e9f925b12dca795f233417e113ec423a`)
-            .then(response => response.json())
-        )
-      )
-      .then(data => {
-        // Guardo toda la data completa en el estado
-        this.setState({ favoritos: data });
-      })
-      .catch(error => console.log(error));
+      let resultados = [];
+      let total = favoritosGuardados.length;
+      //Hago un fetch por cada elemento en favoritos
+      favoritosGuardados.map(favor => {
+        fetch(`https://api.themoviedb.org/3/${favor.tipo}/${favor.id}?api_key=e9f925b12dca795f233417e113ec423a`)
+          .then(res => res.json())
+          .then(data => {
+            resultados.push(data);
+
+            // Cuando terminan todos los fetch
+            if (resultados.length === total) {
+              this.setState({ favoritos: resultados });
+            }
+          })
+          .catch(error => console.log(error));
+      });
     }
 
-    console.log(storage)
+    console.log(storage);
   }
 
-  // Hago al funcion para eliminar de favoritos una pelicula
   eliminarFavorito(id) {
-
-    // Traigo lo que hay en storage
     let storage = localStorage.getItem("favoritos");
     let favoritos = storage !== null ? JSON.parse(storage) : [];
 
-    // Filtro el array para eliminar el id
     let nuevos = favoritos.filter(i => i.id !== id);
 
-    // Guardo todo ya filtrado otra vez en el Storage para podes usarlo en otra screen. 
     localStorage.setItem("favoritos", JSON.stringify(nuevos));
 
-    // También lo saco del estado para que se actualice la vista
     let nuevosState = this.state.favoritos.filter(i => i.id !== id);
     this.setState({ favoritos: nuevosState });
   }
 
   render() {
-    // Itero el estado y los que son tipo peliculas los meto en una variable, los tipos tv en otra. 
-    //Estos despues los voy a iterar y renderizar pasando informacion a los componentes hijos.
     const peliculas = this.state.favoritos.filter(i => i.title);
     const series = this.state.favoritos.filter(i => i.name);
 
     return (
       <>
-        <Header/>
+        <Header />
         <div>
           <h2>Películas favoritas</h2>
 
-          {/* Mapeo la variable películas y le paso info al componente DetalleCard. Tambien le paso la info del id a eliminar para el boton */}
           {peliculas.map(pelicula => (
-          <DetalleCard 
-              key={pelicula.id} 
-              data={pelicula} 
-              agregar={() => this.eliminarFavorito(pelicula.id)} 
+            <DetalleCard
+              key={pelicula.id}
+              data={pelicula}
+              agregar={() => this.eliminarFavorito(pelicula.id)}
               textoBoton="Quitar de favoritos"
-          />
+            />
           ))}
 
           <h2>Series favoritas</h2>
 
-          {/* Mapeo la variable series y le paso la iformacion al componente DetalleCard. Tambien le paso la info del id a eliminar */}
           {series.map(serie => (
-          <DetalleCard 
-              key={serie.id} 
-              data={serie} 
-              agregar={() => this.eliminarFavorito(serie.id)} 
+            <DetalleCard
+              key={serie.id}
+              data={serie}
+              agregar={() => this.eliminarFavorito(serie.id)}
               textoBoton="Quitar de favoritos"
-          />
+            />
           ))}
         </div>
       </>
